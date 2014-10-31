62b2f8e39bacf5725b9490dcb940ad58bab56cf3
emaphp <emmanuel.antico@gmail.com>
2014-10-31 17:48:52 -0300

    styles in notes. order notes by creation date

--- app/notes/models.js
+++ app/notes/models.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone');
 var Config = require('../config.js');
+var moment = require('moment');
 
 var Note = Backbone.Model.extend({
     urlRoot: Config.Notes.endPoint
@@ -7,7 +8,12 @@ var Note = Backbone.Model.extend({
 
 var NotesCollection = Backbone.Collection.extend({
     model: Note,
-    url: Config.Notes.endPoint
+    url: Config.Notes.endPoint,
+    comparator: function (n1, n2) {
+        var t1 = moment(n1.get('createdAt')).format('X');
+        var t2 = moment(n2.get('createdAt')).format('X');
+        return t2 - t1;
+    }
 });
 
 module.exports = {
